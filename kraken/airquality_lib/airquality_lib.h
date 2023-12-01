9527c2075d720d6c9818ecd89b6578f94fa93a91
jggurgel-dev <71473363+jggurgel-dev@users.noreply.github.com>
2023-12-01 00:41:10 -0300

    update airquality lib

--- kraken/airquality_lib/airquality_lib.h
+++ kraken/airquality_lib/airquality_lib.h
@@ -7,7 +7,7 @@
 
 using namespace std;                   // Permite usar string diretamente ao invés de std::string
 
-namespace devtitans::airquality {       // Pacote Airquality
+namespace devtitans::airquality{       // Pacote AirQuality
 
 class AirQuality {
     public:
@@ -36,11 +36,45 @@ class AirQuality {
         int getPM25();
         int getDHT();
         int getMQ();
+        
+        /**
+         * Acessa o nível de luminosidade atual conforme reportado
+         * pelo sensor de luz -- LDR (light-dependent resistor).
+         *
+         * Retorna:
+         *      0: completamente escuro
+         *      1 a 99: nível de luminosidade
+         *      100: completamente claro
+         */
+        //int getLuminosity();
+
+        /**
+         * Threshold - Limiar de luminosidade para ligar/desligar o led.
+         * Ao executar esse método, o dispositivo AirQuality entra no modo
+         * automático, em que o led será ligado se a luminosidade for
+         * menor que o limiar. Caso contrário, o led é desligado.
+         *
+         * Valor retornado/setado:
+         *      0: completamente escuro
+         *      1 a 99: nível de luminosidade
+         *      100: completamente claro
+         */
+        //int getThreshold();
+        //bool setThreshold(int thresholdValue);
 
     private:
-       
+        /**
+         * Métodos para ler e escrever valores nos arquivos "led",
+         * "ldr" ou "threshold" do diretório /sys/kernel/airquality.
+         */
         int readFileValue(string file);
+        //bool writeFileValue(string file, int value);
 
+        /**
+         * Armazena valores simulados para o caso do dispositivo não estar
+         * conectado, mas a propriedade devtitans.airquality.allow_simulated
+         * for true.
+         */
         int simPM10Value = 1180;
         int simPM25Value = 1950;
         int simDHTValue = 1180;
